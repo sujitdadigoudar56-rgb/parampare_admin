5342a4ff884d5a6e42069ec0c64c9dd6c9239e86
SujitD56 <167794487+SujitD56@users.noreply.github.com>
2026-02-22 22:35:39 +0530

    fixing the error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const target = env.VITE_API_URL || 'https://paramparebackend.vercel.app';
+  const target = env.VITE_API_URL
 
   return {
     plugins: [react()],
